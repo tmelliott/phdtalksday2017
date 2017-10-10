67256a566f097ee5d7e9ff70863fbd4a408127f9
Tom Elliott <tomelliottnz@gmail.com>
2017-10-11 11:02:48 +1300

    Initial commit

---
+++ assets/js/scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  // call functions etc here
+  console.log("The page is loaded.");
+
+  Reveal.initialize({
+    math: {
+      jax: ["output/CommonHTML"],
+  		// mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
+  		config: 'TeX-AMS_HTML-full'  // See http://docs.mathjax.org/en/latest/config-files.html
+  	},
+    dependencies: [
+  		{ src: 'assets/revealjs/plugin/math/math.js', async: true }
+  	]
+  });
+});
